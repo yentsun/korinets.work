1e9df39125e67440e4e5e47bab7cf83cdc532ff5
Maksim Korinets <mkorinets@gmail.com>
2024-03-22 17:11:14 +0700

    Fix  Cannot read properties of undefined (reading 'followers')

--- src/components/Cards/GitHub.jsx
+++ src/components/Cards/GitHub.jsx
@@ -8,7 +8,7 @@ export default function GitHub() {
 
     return <Card isLoading={ ! yentsun } thumb="github.gif"
                  major={`repos: ${yentsun?.public_repos}` }
-                 minor={ `followers: ${yentsun.followers}` }
+                 minor={ `followers: ${yentsun?.followers}` }
                  content={ lastEvent
                      ? `${lastEvent.name}: [${lastEvent.type}] ${lastEvent.text} (${lastEvent.createdAgo} ago)`
                      : null } />
